95cbe2f53f1ccc814b2b25b404aca5ad83f27de7
SvitlanaTurych <svitlana.study24@gmail.com>
2025-05-24 21:55:58 +0300

    updated props in MovieList

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,6 +1,6 @@
 import MovieCard from './MovieCard';
 
-function MovieList({ movies }) {
+function MovieList({ movies, onNavigateToBooking }) {
   if (movies.length === 0) {
     return <div className="no-results">Фільмів не знайдено</div>;
   }
@@ -8,7 +8,7 @@ function MovieList({ movies }) {
   return (
     <div className="movie-grid">
       {movies.map(movie => (
-        <MovieCard key={movie.id} movie={movie} />
+        <MovieCard key={movie.id} movie={movie} onNavigateToBooking={onNavigateToBooking} />
       ))}
     </div>
   );
